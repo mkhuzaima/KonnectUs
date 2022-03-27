5b1acce56fb3a09ad9f0df977a5a379f21abdaaa
Muhammad Awais Yousaf Butt <buttawa0000@gmail.com>
2022-03-27 10:58:49 +0500

    Default_page
    Default page has been made.

--- KonnectUs/KonnectUs/App_Start/BundleConfig.cs
+++ KonnectUs/KonnectUs/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace KonnectUs
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/res").Include(
+                     "~/Scripts/res.js"));
         }
     }
 }
